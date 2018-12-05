6a8aa934f8dce7aeff71a9c3477e6a6acf4fe332
Tom Bech <tom.bech@gmail.com>
2018-12-05 21:39:43 +0100

    More testing

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 echo "Installing"
 
-python --version
-
 if [ "$TRAVIS_OS_NAME" == "osx"   ] ; then
     echo "Building on OSX.."
+
+    brew install python3
+    # make brews python the system default
+    brew link --overwrite python3
+
+    python --version
 fi
 
 if [ "$TRAVIS_OS_NAME" == "linux"   ] ; then
     echo "Building on Linux.."
+
+    python --version
 fi
