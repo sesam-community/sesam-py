9ae583fab158dbcb4ecee2cabf64d3165ac12956
Tom Bech <tom.bech@gmail.com>
2018-12-06 09:53:45 +0100

    Where's the file..

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ if [ -n "$TRAVIS_TAG" ] ; then
     echo "Packaging..."
     export SESAM_ARTIFACT_NAME=${TRAVIS_BUILD_DIR}/sesam-${TRAVIS_OS_NAME}-${TRAVIS_TAG}-${TRAVIS_BUILD_NUMBER}.tar.gz
     tar -zcf ${SESAM_ARTIFACT_NAME} dist/sesam
+    echo "Created artifact:"
+    ls -al ${SESAM_ARTIFACT_NAME}
 fi
