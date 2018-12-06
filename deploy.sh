0dc7a063f733590d58dff5397da92870a421222c
Tom Bech <tom.bech@gmail.com>
2018-12-06 10:50:32 +0100

    Drop subfolders in artifact

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 if [ -n "$TRAVIS_TAG" ] ; then
     echo "Packaging..."
     export SESAM_ARTIFACT_NAME=${TRAVIS_BUILD_DIR}/sesam-${TRAVIS_OS_NAME}-${TRAVIS_TAG}-${TRAVIS_BUILD_NUMBER}.tar.gz
-    tar -zcf ${SESAM_ARTIFACT_NAME} dist/sesam
+    pushd dist
+    tar -zcf ${SESAM_ARTIFACT_NAME} sesam
+    popd
     echo "Created artifact:"
     ls -al ${SESAM_ARTIFACT_NAME}
 fi
