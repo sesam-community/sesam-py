a7065a447d306619147c6b177c7f2a0a5d99ee6d
Tom Bech <tom.bech@gmail.com>
2019-03-14 14:24:34 +0100

    Bump version

--- sesam.py
+++ sesam.py
@@ -24,7 +24,7 @@ from fnmatch import fnmatch
 from decimal import Decimal
 import pprint
 
-sesam_version = "1.15.18"
+sesam_version = "1.15.19"
 
 logger = logging.getLogger('sesam')
 LOGLEVEL_TRACE = 2
