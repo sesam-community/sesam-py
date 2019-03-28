7bb89b7e5bb68cb9fa53e21310e5b535f8fedadf
Tom Bech <tom.bech@gmail.com>
2019-03-28 08:47:41 +0100

    Rework travis artifact subdir

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,22 @@
 
 if [ -n "$TRAVIS_TAG" ] ; then
     echo "Packaging..."
+    mkdir artifact
+
     pushd dist
 
     if [ "$TRAVIS_OS_NAME" == "linux"   ] || [ "$TRAVIS_OS_NAME" == "osx"   ] ; then
-        export SESAM_ARTIFACT_NAME=${TRAVIS_BUILD_DIR}/sesam-${TRAVIS_OS_NAME}-${TRAVIS_TAG}.tar.gz
+        export SESAM_ARTIFACT_NAME=${TRAVIS_BUILD_DIR}/artifact/sesam-${TRAVIS_OS_NAME}-${TRAVIS_TAG}.tar.gz
         tar -zcf ${SESAM_ARTIFACT_NAME} sesam
     fi
 
     if [ "$TRAVIS_OS_NAME" == "windows"   ] ; then
-        export SESAM_ARTIFACT_NAME=${TRAVIS_BUILD_DIR}/sesam-${TRAVIS_OS_NAME}-${TRAVIS_TAG}.zip
+        export SESAM_ARTIFACT_NAME=${TRAVIS_BUILD_DIR}/artifact/sesam-${TRAVIS_OS_NAME}-${TRAVIS_TAG}.zip
         7z a ${SESAM_ARTIFACT_NAME} sesam.exe
     fi
 
     popd
+
     echo "Created artifact:"
     ls -al ${SESAM_ARTIFACT_NAME}
 fi
