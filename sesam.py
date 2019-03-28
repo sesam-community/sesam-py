71d9c87dda15c5cb49bd451c0e07117c711083b5
Tom Bech <tom.bech@gmail.com>
2019-03-28 15:11:13 +0100

    Try omitting  in deploy

--- sesam.py
+++ sesam.py
@@ -24,7 +24,7 @@ from fnmatch import fnmatch
 from decimal import Decimal
 import pprint
 
-sesam_version = "1.15.31"
+sesam_version = "1.15.33"
 
 logger = logging.getLogger('sesam')
 LOGLEVEL_TRACE = 2
