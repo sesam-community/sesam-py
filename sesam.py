8e031ef0a2dfc50bc6a3f50f1b4a36f06a73091f
Tom Bech <tom.bech@bouvet.no>
2019-03-29 10:12:17 +0100

    Forgot to bump latest version

--- sesam.py
+++ sesam.py
@@ -24,7 +24,7 @@ from fnmatch import fnmatch
 from decimal import Decimal
 import pprint
 
-sesam_version = "1.15.34"
+sesam_version = "1.15.35"
 
 logger = logging.getLogger('sesam')
 LOGLEVEL_TRACE = 2
