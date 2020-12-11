23ca09d94e781021b50b715472a58b4dac418554
Branislav Jenco <branislav.jenco@sesam.io>
2020-12-11 13:55:45 +0100

    [IS-10255-2] more reorg

--- install-gh.sh
+++ install-gh.sh
@@ -9,12 +9,3 @@ pip install --user -U -r requirements.txt
 
 pyinstaller --onefile sesam.py
 
-if [ "$RUNNER_OS" == "Linux" ] || [ "$RUNNER_OS" == "macOS" ] ; then
-    export SESAM_CLIENT=$PWD/dist/sesam
-fi
-
-if [ "$RUNNER_OS" == "Windows" ] ; then
-    export SESAM_CLIENT=$PWD/dist/sesam.exe
-fi
-
-$SESAM_CLIENT -h
