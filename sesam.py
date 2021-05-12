50905fdbd41df8a64710b256cb6d530af13f0597
Branislav Jenco <branislav.jenco@sesam.io>
2021-05-12 09:10:24 +0200

    [IS-11075] Bumped version properly

--- sesam.py
+++ sesam.py
@@ -26,7 +26,7 @@ from decimal import Decimal
 import pprint
 from jsonformat import format_object, FormatStyle
 
-sesam_version = "2.2.4"
+sesam_version = "2.2.5"
 
 logger = logging.getLogger('sesam')
 LOGLEVEL_TRACE = 2
