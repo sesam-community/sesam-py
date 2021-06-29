23b91d2d70feb6ed11453bbe040aaa6a085d3121
Branislav Jenco <branislav.jenco@sesam.io>
2021-06-29 14:57:07 +0200

    [IS-10255] Fixing up hostname of new ci node and jwt

--- test-gh.sh
+++ test-gh.sh
@@ -14,7 +14,7 @@ fi
 
 $SESAM_CLIENT -h
 
-export NODE_URL=https://datahub-cd9f97d6.sesam.cloud/api
+export NODE_URL=https://datahub-fff057e8.sesam.cloud/api
 export PUBLIC_CI_TOKEN=$SESAM_TOKEN
 
 pushd tests
