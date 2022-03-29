c649b3336f4584fdbb749616f809986abb2bf854
JonasAls89 <jonas.christensen@sesam.io>
2022-03-29 09:28:50 +0200

    Update sesam.py

--- sesam.py
+++ sesam.py
@@ -26,7 +26,7 @@ from decimal import Decimal
 import pprint
 from jsonformat import format_object, FormatStyle
 
-sesam_version = "2.2.14"
+sesam_version = "2.2.15"
 
 logger = logging.getLogger('sesam')
 LOGLEVEL_TRACE = 2
