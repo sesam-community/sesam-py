8b0d60ce23160ecba56dd1f6c16f863ab29df71b
Tom Bech <tom.bech@sesam.io>
2022-11-11 15:53:36 +0100

    IS-13415: increase log level of tests

--- tests/embedded_conversion_test/run_test.sh
+++ tests/embedded_conversion_test/run_test.sh
@@ -13,13 +13,13 @@ pushd test_after
 $SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv wipe
 
 # First, convert
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -v convert
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -vv convert
 
 # Then, test if it works to upload (along with testdata), run, and verify
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v -use-internal-scheduler -print-scheduler-log test
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv -use-internal-scheduler -print-scheduler-log test
 
 # Clean up completely after a run
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v reset
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv reset
 
 popd
 
--- tests/fair_weather_test/run_test.sh
+++ tests/fair_weather_test/run_test.sh
@@ -5,14 +5,14 @@ set -e
 echo "Running fairweather tests.."
 
 # Start with a clean slate to avoid "singlenode" issues from any other tests
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v reset
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v -print-scheduler-log -whitelist-file whitelist.txt test
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v update
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v status
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -dump -v download -sesamconfig-file .mysesamconfig.json
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv reset
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv -print-scheduler-log -whitelist-file whitelist.txt test
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv update
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv status
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -dump -vv download -sesamconfig-file .mysesamconfig.json
 
 # Clean up completely after a run
-$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -v reset
+$SESAM_CLIENT -node $NODE_URL -jwt $PUBLIC_CI_TOKEN -skip-tls-verification -vv reset
 
 if [ ! -f "sesam-config.zip" ]; then
     echo "Test of download failed!"
