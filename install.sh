d2736d20e51c0dfc823752add8e2dc00aa92f475
parhambarazesh <parham.barazesh@gmail.com>
2023-02-14 11:01:39 +0100

    add support for data files in Windows and Linux/Mac

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -x
 
-echo "Installing.."
+echo "Installing required packages.."
 
 pip install pyinstaller
 
 pip install --user -U -r requirements.txt
 
-pyinstaller --onefile --add-data "connector_cli:connector_cli" sesam.py
+OS="`uname`"
+if [ "$OS" = "Linux" ] || [ "$OS" = "Darwin" ]; then
+  echo "Installing sesam-py on Linux/Mac"
+  pyinstaller --onefile --add-data "connector_cli:connector_cli" sesam.py
+elif [ "$OS" = "WindowsNT" ]; then
+  echo "Installing sesam-py on Windows"
+  pyinstaller --onefile --add-data "connector_cli;connector_cli" sesam.py
+fi
