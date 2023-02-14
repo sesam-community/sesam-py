d076fdeff3335920b50106d07e261d24947a6876
parhambarazesh <parham.barazesh@gmail.com>
2023-02-14 13:08:17 +0100

    test windows build. minor change

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ OS="`uname`"
 if [ "$OS" = "Linux" ] || [ "$OS" = "Darwin" ]; then
   echo "Installing sesam-py on Linux/Mac"
   pyinstaller --onefile --add-data "connector_cli:connector_cli" sesam.py
-elif [ "$OS" = CYGWIN_NT-* ] || [ "$OS" = MINGW64_NT-*]; then
+else
   echo "Installing sesam-py on Windows"
   pyinstaller --onefile --add-data "connector_cli;connector_cli" sesam.py
 fi
