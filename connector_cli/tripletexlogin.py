49b2802e34bc24ca44f53b41969322b2d4fdc786
parhambarazesh <parham.barazesh@gmail.com>
2023-02-16 21:10:24 +0100

    show error when updating env variables and secrets fail

--- connector_cli/tripletexlogin.py
+++ connector_cli/tripletexlogin.py
@@ -32,21 +32,29 @@ def login_via_tripletex(args):
         }
         # post secrets
         for secret, value in secrets.items():
-            requests.post(service_url + "/systems/%s/secrets" % system_placeholder,
-                          headers={"Authorization": "Bearer %s" % service_jwt}, json={secret: value})
-            print("Updated secret: %s" % secret)
+            response = requests.post(service_url + "/systems/%s/secrets" % system_placeholder,
+                                headers={"Authorization": "Bearer %s" % service_jwt}, json={secret: value})
+            if response.status_code == 200:
+                print("Updated secret: %s" % secret)
+            else:
+                print("Failed to update secret: %s" % secret)
+                print(response.text)
 
         # update env
         env = requests.get(service_url + "/env", headers={"Authorization": "Bearer %s" % service_jwt}).json()
         env["base_url"] = base_url
-        if os.path.isfile(".additionalprops"):
-            with open(".params", "r") as f:
-                for line in f.readlines():
-                    key, value = line.split("=")
-                    env[key] = value.strip()
+        # if os.path.isfile(".additionalprops"):
+        #     with open(".additionalprops", "r") as f:
+        #         for line in f.readlines():
+        #             key, value = line.split("=")
+        #             env[key] = value.strip()
 
-        requests.put(service_url + "/env", headers={"Authorization": "Bearer %s" % service_jwt}, json=env)
-        print("Updated environment variables")
-        print("Secrets and env has been updated, now go and do your development!")
+        response=requests.put(service_url + "/env", headers={"Authorization": "Bearer %s" % service_jwt}, json=env)
+        if response.status_code == 200:
+            print("Updated environment variables")
+            print("Secrets and env has been updated, now go and do your development!")
+        else:
+            print("Failed to update environment variables")
+            print(response.text)
     else:
         print("Missing arguments, please provide all required arguments")
