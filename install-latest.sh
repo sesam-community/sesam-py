2110993029da888d816facdabba991b9482b7457
parhambarazesh <parham.barazesh@gmail.com>
2023-02-22 15:53:34 +0100

    bump version to 2.5.10

--- install-latest.sh
+++ install-latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TAG=${SESAM_TAG:-2.5.8}
+TAG=${SESAM_TAG:-2.5.10}
 
 wget -O sesam.tar.gz https://github.com/sesam-community/sesam-py/releases/download/$TAG/sesam-linux-$TAG.tar.gz
 tar -xf sesam.tar.gz
