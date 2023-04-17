be19d547d6078537d6641691c65dff39b4a670ed
parhambarazesh <parham.barazesh@gmail.com>
2023-04-17 10:44:50 +0200

    update workflow to use Ubuntu 20.04 (18.04 is deprecated)

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Testing..."
 
-if [ "$OS" == "ubuntu-18.04" ] || [ "$OS" == "macos-latest" ] ; then
+if [ "$OS" == "ubuntu-20.04" ] || [ "$OS" == "macos-latest" ] ; then
     export SESAM_CLIENT=$PWD/dist/sesam
 fi
 
