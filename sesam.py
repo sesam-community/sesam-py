f5ecc6de8eaed76af8098cc90f70eb1af9365bdc
Branislav Jenco <branislav.jenco@sesam.io>
2023-06-12 12:16:40 +0200

    [IS-15126] Bumped version

--- sesam.py
+++ sesam.py
@@ -28,7 +28,7 @@ from requests.exceptions import HTTPError
 from connector_cli import connectorpy, oauth2login, tripletexlogin
 from jsonformat import FormatStyle, format_object
 
-sesam_version = "2.5.30"
+sesam_version = "2.5.31"
 
 logger = logging.getLogger("sesam")
 LOGLEVEL_TRACE = 2
