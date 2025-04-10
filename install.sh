977e25c09ffdef50ec0d11860606ef3268bca903
Michael Maydew <michael.maydew@bouvet.no>
2025-04-10 15:51:51 +0200

    IS-18280: Add tests and build to docker

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ echo "Installing required packages.."
 
 pip install pyinstaller
 
-pip install --user -U -r requirements.txt
-
 OS="`uname`"
 if [ "$OS" = "Linux" ] || [ "$OS" = "Darwin" ]; then
   echo "Installing sesam-py on Linux/Mac"
