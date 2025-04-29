dacb47f1ba1a3526ab37016289fe6bfe93ba1c1d
Michael Maydew <michael.maydew@bouvet.no>
2025-04-29 14:52:44 +0200

    IS-18280: Change assert for test_parse_config_file

--- tests/test_funcs/test_parse_config_file/test_parse_config_file.py
+++ tests/test_funcs/test_parse_config_file/test_parse_config_file.py
@@ -13,7 +13,7 @@ def test_parse_json_config():
         "./tests/test_funcs/test_parse_config_file/config.json"
     )
 
-    assert type(parsed_config) == dict
+    assert isinstance(parsed_config, dict)
     assert parsed_config.get("test", "") == "Some test config"
     assert parsed_config.get("is_testing", "") == "true"
 
@@ -26,6 +26,6 @@ def test_parse_syncconfig():
         "./tests/test_funcs/test_parse_config_file/.syncconfig"
     )
 
-    assert type(parsed_config) == dict
+    assert isinstance(parsed_config, dict)
     assert parsed_config.get("test", "") == "Some test config"
     assert parsed_config.get("is_testing", "") == "true"
